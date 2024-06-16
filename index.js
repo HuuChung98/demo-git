02b0c9f7d70d5a38f61274b252a8b600fec72ff6
chungDev <balechung93@gmail.com>
2024-06-16 18:18:37 +0700

    dfdfdfd

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const arr = [1, 2, 3];
 
-const arr2 = arr.map(item => item * 10);
-
-const arr3 = arr.filter(item => item < 3);
-
-const arr4 = arr.find(item => item < 3);
-
 const ob = {
     name: 'chung'
 }
