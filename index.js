326a191a4cf1b01a73fdd72f8d6fd3ae1a1bb8e0
chungDev <balechung93@gmail.com>
2024-06-16 10:32:09 +0700

    add filter

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const arr = [1, 2, 3];
 
-const arr2 = arr.map(item => item > 1);
+const arr2 = arr.map(item => item * 10);
+
+const arr3 = arr.filter(item => item < 3);
+
+console.log(arr2);
+console.log(arr3);
